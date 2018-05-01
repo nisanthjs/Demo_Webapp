7bab75171f768224d1b2f1afe43351cd93225675
njanard1 <Nisanth.JanardananPillaiSyamakumari1@T-Mobile.com>
2018-05-01 23:54:54 +0530

    POC changes.

--- ansible_scripts/executePlaybook.sh
+++ ansible_scripts/executePlaybook.sh
@@ -5,9 +5,10 @@ BUILD_VERSION=$1
 begin=$(date +"%s")
 
 VERSION="version=$BUILD_VERSION"
+echo -e "VER = $VERSION"
 
 retry=0
-until [ $retry -ge 5 ]
+until [ $retry -ge 3 ]
  do
   ansible-playbook devopspoc.yml --extra-vars $VERSION -vv && break  # substitute your command here
   retry=$[$retry+1]
